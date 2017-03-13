a22d9eacb6bbb0d640244ee8024d427c46b5e177
Timothy Barnard <timothy.barnard@mydit.ie>
2017-03-13 18:10:37 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TBMBaaSApp
---
+++ TBMBaaSApp/UserView.swift
@@ -0,0 +1,21 @@
+//
+//  UserView.swift
+//  TBMBaaSApp
+//
+//  Created by Timothy Barnard on 13/03/2017.
+//  Copyright © 2017 Timothy Barnard. All rights reserved.
+//
+
+import UIKit
+
+class UserView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
